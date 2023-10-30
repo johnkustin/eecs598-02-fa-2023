670ee16bdd8fa833ec4a1a683431ea9f67f71bd2
John Kustin <johnkustin@gmail.com>
2023-10-30 19:37:16 -0400

    can select b/t mid-rise, mid-tread, or the original quantizer. all options give similar results

--- QNS.m
+++ QNS.m
@@ -24,11 +24,11 @@ classdef QNS < handle
             obj.x = 0;
         end
         
-        function  y = step(obj, x)
-            y = stepx(obj, x, true);
+        function  y = step(obj, x, quantizerType)
+            y = stepx(obj, x, true, quantizerType);
         end
                
-        function  y = stepx(obj, x, quantization)
+        function  y = stepx(obj, x, quantization, quantizerType)
             global P M
             % P: order; M: number of levels
             obj.x = x;
@@ -40,7 +40,45 @@ classdef QNS < handle
             
             if quantization
                 level_i = min(max(round(M/4*(obj.mem(i)/obj.L+2)-1/2),0),M-1);
+                level_i_tread = ceil(obj.mem(i)/obj.L - 1/2); % forward quantization stage of mid-tread quantizer
+                level_i_riser = ceil(obj.mem(i)/obj.L) - 1/2; % forward quantization stage of mid-riser quantizer
+                % i thinik max(round(M/4*(obj.mem(i)/obj.L+2)-1/2),0)
+                % implements a "dead zone quantizer"
+                % https://en.wikipedia.org/wiki/Quantization_(signal_processing)#Dead-zone_quantizers
+                % but his deadzone width is same as LSB size (obj.L) so it
+                % is a uniform quantizer
                 y = obj.L*(4/M*(level_i'+1/2)-2);
+                
+                y_riser = obj.L * (level_i_riser + 1/2); % inverse quantization stage of mid-riser 
+                y_tread = obj.L * level_i_tread; % inverse quantization stage of mid-tread
+                if y_riser > obj.L * 3/2 % 
+                    y_riser = obj.L *3/2;
+                elseif y_riser < -obj.L * 3/2
+                    y_riser = obj.L * -3/2;
+                end
+
+                if y_tread > obj.L * 3/2 % 
+                    y_tread = obj.L *3/2;
+                elseif y_tread < -obj.L * 3/2
+                    y_tread = obj.L * -3/2;
+                end
+
+%                 my_level_i = 1/2 + ceil(obj.mem(i)./obj.L);
+%                 my_y = obj.L * (my_level_i + 1/2);
+%                 my_y = obj.L * my_level_i;
+%                 if y_my ~= y
+%                     sprintf("y=%f my_y=%f", y, y_my)
+% %                     sprintf("L=%f obj.mem(i)=%f", obj.L, obj.mem(i))
+%                 end
+                if quantizerType == "mid-riser"
+                    y = y_riser;
+                elseif quantizerType == "mid-tread"
+                    y = y_tread;
+                elseif quantizerType == "orig"
+                    % do nothing. y = y
+                end
+                
+                
             else
                 y = obj.mem(P);
             end
