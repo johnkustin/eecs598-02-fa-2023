7bbac0086d1f253c6814f41ad3f7fe39a3a5417d
Ravi Somvanshi <rsomvan@umich.edu>
2023-12-04 18:49:16 -0500

    synthesis LPD

--- verilog/LPD/scripts/constraints.tcl
+++ verilog/LPD/scripts/constraints.tcl
@@ -15,20 +15,10 @@ set_clock_latency $clock_latency [get_clocks core_clk]
 set_load 0.3 [all_outputs]
 set_driving_cell -no_design_rule -lib_cell NBUFFX4_RVT [all_inputs]
 
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" valid_u_in*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" valid_u_in*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" data_u_in*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" data_u_in*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" valid_e_in*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" valid_e_in*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" data_e_in*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" data_e_in*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" write_lut_in*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" write_lut_in*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" write_lut_data*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" write_lut_data*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" write_lut_idx*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" write_lut_idx*] -clock core_clk
+set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" valid_in*] -clock core_clk
+set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" valid_in*] -clock core_clk
+set_input_delay -max [expr $clock_period * 0.2] [get_ports -filter "direction == in" data_in*] -clock core_clk
+set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" data_in*] -clock core_clk
 set_output_delay -max [expr $clock_period * 0.5] [get_ports -filter "direction == out" data_out*] -clock core_clk
 set_output_delay -min [expr $clock_period * 0.4] [get_ports -filter "direction == out" data_out*] -clock core_clk
 set_output_delay -max [expr $clock_period * 0.3] [get_ports -filter "direction == out" valid_out*] -clock core_clk
